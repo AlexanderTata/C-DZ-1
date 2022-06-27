af5e74cdc652dad433396fcac45c17772e4c0bb3
Alexander Tata <ta1996@yandex.ru>
2022-06-27 18:07:34 +0300

    Решение второй задачи

--- Program.cs
+++ Program.cs
@@ -20,7 +20,36 @@ else
     Console.WriteLine ("Число min == second");
 }
 
-/*Задача 6: Напишите программу, которая на вход принимает число и выдаёт, является ли число чётным (делится ли оно на два без остатка).
+/*Задача №2: Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+2, 3, 7 -> 7
+44 5 78 -> 78
+22 3 9 -> 22*/
+{   Console.WriteLine("Введите первое число ");
+    int numberOne = Convert.ToInt32(Console.ReadLine());
+    Console.WriteLine("Введите второе число ");
+    int numberTwo = Convert.ToInt32(Console.ReadLine());
+    Console.WriteLine("Введите третье число ");
+    int numberThree = Convert.ToInt32(Console.ReadLine());
+    int max = numberOne;
+    if (numberTwo > numberOne)
+    {
+        max = numberTwo;
+    }
+    else 
+        {
+        max = numberOne;
+        } 
+    if (numberThree < max)
+        {
+        max = numberThree;
+        }
+    else
+        {
+        max = numberOne;
+        } 
+    Console.WriteLine(max);
+}
+/*Задача 3: Напишите программу, которая на вход принимает число и выдаёт, является ли число чётным (делится ли оно на два без остатка).
 4 -> да
 -3 -> нет
 7 -> нет*/
